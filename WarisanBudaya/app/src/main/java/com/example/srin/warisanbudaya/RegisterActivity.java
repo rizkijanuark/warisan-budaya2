a20f59b410bf0f95de5e98fca14f8dbc8538e5a6
Mursil Shadruddin Afkar <afkarshad@gmail.com>
2016-05-21 17:54:51 +0700

    tes link snk

--- WarisanBudaya/app/src/main/java/com/example/srin/warisanbudaya/RegisterActivity.java
+++ WarisanBudaya/app/src/main/java/com/example/srin/warisanbudaya/RegisterActivity.java
@@ -1,22 +1,34 @@
 package com.example.srin.warisanbudaya;
 
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.*;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by SRIN on 4/11/2016.
  */
 public class RegisterActivity extends AppCompatActivity {
-    Button reg;
     EditText nmdepan, nmblkg, email, pwd1, pwd2, telp;
     CheckBox snk;
     DBHelper mydb;
-    TextView klik;
+    TextView klik, snkLink;
     Validator val = new Validator();
+    ProgressDialog downloadProgressDialog;
+    final String DOWNLOAD_URL = "http://www.nus.edu.sg/comcen/gethelp/guide/itcare/wireless/NUS-WPA2%20Network%20Configuration%20Guide%20for%20Android%204.0.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.registrasi);
 
         mydb = new DBHelper(this);
+        downloadProgressDialog = new ProgressDialog(this);
+
+        downloadProgressDialog.setMessage("Mengunduh Syarat & Ketentuan");
+        downloadProgressDialog.setIndeterminate(true);
+        downloadProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        downloadProgressDialog.setCancelable(true);
+
+
 
         nmdepan = (EditText) findViewById(R.id.etNamaDepan);
         nmblkg = (EditText) findViewById(R.id.etNamaBelakang);
@@ -35,12 +55,28 @@ public class RegisterActivity extends AppCompatActivity {
         snk = (CheckBox) findViewById(R.id.cbSnK);
 
         klik = (TextView) findViewById(R.id.tvKlik);
+        snkLink = (TextView) findViewById(R.id.tvSnK);
 
         klik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 finish();
+            }
+        });
+
+        snkLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DownloadSNK downloadTask = new DownloadSNK(RegisterActivity.this);
+                downloadTask.execute(DOWNLOAD_URL);
+
+                downloadProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        downloadTask.cancel(true);
+                    }
+                });
             }
         });
     }
@@ -107,5 +143,129 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+
+    private class DownloadSNK extends AsyncTask<String, Integer, String>{
+
+        private Context context;
+        private PowerManager.WakeLock mWakeLock;
+        private String FILE_NAME = "syarat dan ketentuan WBI.pdf";
+        private String FOLDER_NAME = "/sdcard/wbi/";
+        private File outputFile;
+
+        public DownloadSNK(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    return "Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage();
+                }
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                File wbiDir = new File(FOLDER_NAME);
+                wbiDir.mkdirs();
+                outputFile = new File(wbiDir, FILE_NAME);
+                output = new FileOutputStream(outputFile);
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while ((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if (isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if (fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                try {
+                    if (output != null)
+                        output.close();
+                    if (input != null)
+                        input.close();
+                } catch (IOException ignored) {
+                }
+
+                if (connection != null)
+                    connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    getClass().getName());
+            mWakeLock.acquire();
+            downloadProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            downloadProgressDialog.setIndeterminate(false);
+            downloadProgressDialog.setMax(100);
+            downloadProgressDialog.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            mWakeLock.release();
+            downloadProgressDialog.dismiss();
+
+            if (s!=null){
+                Toast.makeText(context, "Download failed: " + s, Toast.LENGTH_LONG).show();
+            } else {
+                openFile(outputFile);
+            }
+        }
+    }
+
+    private void openFile(final File file){
+        new Thread(new Runnable() {
+            public void run() {
+                Uri path = Uri.fromFile(file);
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(path, "application/pdf");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(RegisterActivity.this, "PDF Reader application is not installed in your device", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).start();
     }
 }
